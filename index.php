4ef00a70a7d47b7c358ab22baad9049aff0c0989
xqwtxon <xqwtxon@hotmail.com>
2023-09-13 04:22:25 +0000

    Test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="icon" type="image/png" href="assets/server-logo.png">
-      <meta name="og:title" content="PrideMC Network">
+      <meta name="og:title" content="PrideMC Network"> 
       
 		<title>Home ─ PrideMC Network</title>
         <style>
