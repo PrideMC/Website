2e76bcc5d7cba04e7f954bfefd97aadac2737884
xqwtxon <xqwtxon@hotmail.com>
2023-09-13 04:17:58 +0000

    Test

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="icon" type="image/png" href="assets/server-logo.png">
 		<title>Error ─ PrideMC Network</title>
-        <style>
+        <style> 
             html {
 				color: #fff;
 				background-color: #424242;
